055fa6d43b72c2ad74b0c8f15f4d6cb6d33fc3fd
George Walker <george@georgewwalker.com>
2017-06-09 17:53:48 -0500

    Add summary to Aeroqual API

---
+++ api/aeroqual_o3/summary/index.php
@@ -0,0 +1,39 @@
+<?php
+
+// Import the keys/secret variables
+include("../../keys.php");
+
+// Set the header Content-Type for JSON
+header('Content-Type: application/json');
+
+// Open connection to database using variables set in keys
+$dbconn = pg_connect("host=" . $dbhost . " port=". $dbport . " dbname=" . $dbname . " user=" . $dbuser . " password=" . $dbpass) or die(return_error("Could not connect to database.", pg_last_error()));
+
+// Get the device ID from the URL parameter
+$device = $_GET['device'];
+
+// Get the season from the URL parameter
+$season = $_GET['season'];
+
+// Build the SQL query
+$query = 'SELECT DATE(date), avg(o3ppm) as average, max(o3ppm) as max, min(o3ppm) as min FROM aeroqualno2 WHERE unit_id = $1 AND season = $2 GROUP BY DATE(date) ORDER BY DATE(date)';
+
+// Run the query
+$result = pg_query_params($dbconn, $query, array($device, $season)) or die (return_error("Query failed.", pg_last_error()));
+
+// Create JSON result
+$resultArray = pg_fetch_all($result);
+
+// Encode the array as JSON and return it.
+echo json_encode($resultArray);
+
+// Free resultset
+pg_free_result($result);
+
+// Closing connection
+pg_close($dbconn);
+
+function return_error($error_description, $error_details) {
+	return '{"error":"' . $error_description . '","error_details":"' . $error_details .'"}';
+}
+?>
