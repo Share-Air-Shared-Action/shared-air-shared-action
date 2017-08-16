77b36d94c4af62a40ff0269157ce75cbd70c1410
George W. Walker <george@georgewwalker.com>
2017-08-16 10:35:57 -0500

    Updated PurpleAir ids API

--- api/purpleairprimary_pm1.0/ids/index.php
+++ api/purpleairprimary_pm1.0/ids/index.php
@@ -13,7 +13,7 @@ $dbconn = pg_connect("host=" . $dbhost . " port=". $dbport . " dbname=" . $dbnam
 $season = $_GET['season'];
 
 // Build the SQL query
-$query = 'SELECT DISTINCT purpleairprimary.device_name as device, stationarylocations.latitude, stationarylocations.longitude, stationarylocations.community FROM purpleairprimary INNER JOIN stationarylocations ON (purpleairprimary.device_name = stationarylocations.unit_id) WHERE purpleairprimary.season = $1';
+$query = 'SELECT DISTINCT purpleairprimary.device_name as device, purpleairprimary.community, stationarylocations.latitude, stationarylocations.longitude FROM purpleairprimary LEFT JOIN stationarylocations ON (purpleairprimary.device_name = stationarylocations.unit_id) WHERE purpleairprimary.season = $1';
 
 // Run the query
 $result = pg_query_params($dbconn, $query, array($season)) or die (return_error("Query failed.", pg_last_error()));
